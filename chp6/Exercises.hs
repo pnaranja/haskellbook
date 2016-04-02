114aa99f4ff87446600a4de98dc7c31296c1b810
pnaranja <paul.naranja@gmail.com>
2016-04-02 11:30:33 -0700

    Finished Intermission Exercises for chp 6

---
+++ chp6/Exercises.hs
@@ -0,0 +1,40 @@
+
+main :: IO ()
+main = undefined
+
+-- Intermission Exercises
+data TisAnInteger = TisAnInteger Integer
+instance Eq TisAnInteger where
+    (==) (TisAnInteger a) (TisAnInteger a') = a == a'
+
+data TwoIntegers = TwoIntegers Integer Integer
+instance Eq TwoIntegers where
+    (==) (TwoIntegers a b) (TwoIntegers a' b') = a == a' && b == b'
+
+data StringOrInt = TisAnInt Int | TisAString String
+instance Eq StringOrInt where
+    (==) (TisAnInt a) (TisAnInt a') = a == a'
+    (==) (TisAString a) (TisAString a') = a == a'
+    (==) _ _ = False
+
+data Pair a = Pair a a
+instance (Eq a) => Eq (Pair a) where
+    (==) (Pair a b) (Pair a' b') = a == a' && b == b'
+
+data Tuple a b = Tuple a b
+instance (Eq a, Eq b) => Eq (Tuple a b) where
+    (==) (Tuple a b) (Tuple a' b') = a == a' && b == b'
+
+data Which a = ThisOne a | ThatOne a
+instance (Eq a) => Eq (Which a) where
+    (==) (ThisOne a) (ThisOne a') = a == a'
+    (==) (ThatOne a) (ThatOne a') = a == a'
+    (==) (ThisOne a) (ThatOne a') = a == a'
+    (==) (ThatOne a) (ThisOne a') = a == a'
+
+data EitherOr a b = Hello a | Goodbye b
+instance (Eq a, Eq b) => Eq (EitherOr a b) where
+    (==) (Hello a) (Hello a') = a == a'
+    (==) (Goodbye b) (Goodbye b') = b == b'
+    (==) (Hello _) (Goodbye _) = False
+    (==) (Goodbye _) (Hello _) = False
