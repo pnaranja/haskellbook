d3f9194b5805b9e46be1218a60d602574c812141
pnaranja <paul.naranja@gmail.com>
2016-05-04 22:30:58 -0700

    Chapter 8 in progress

--- chp8/Exercises.hs
+++ chp8/Exercises.hs
@@ -4,7 +4,7 @@ main = undefined
 
 incTimes :: (Eq a, Num a) => a -> a -> a
 incTimes 0 n = n
-incTimes times n = (incTimes (times-1) n) + 1
+incTimes times n = 1 + (incTimes (times-1) n)
 
 --Extract recursion of incTimes
 applyTimes :: (Eq a, Num a) => a -> (b -> b) -> b -> b
@@ -18,4 +18,18 @@ incTimes' times n = applyTimes times (+1) n
 --Write out evaluation of applyTimes 5 (+1) 5
 -- ((+1) ((+1) ((+1) ((+1) ((+1) 5)))))
 -- (+1) . (+1) . (+1) . (+1) . (+1) $ 5
+
+
+-- bottom: Computations that do not result in a value
+-- because it failed with an error or does not terminate
+-- i.e. let x = x in x
+
+-- i.e. Anything that throws an exception means no real value returned
+f_bottom :: Bool -> Int
+f_bottom True = error "Going to throw Exception"
+f_bottom False = 0
+
+-- i.e: Partial function - does not take into account f_bottom True
+f_bottom2 :: Bool -> Int
+f_bottom2 False = 0
 
