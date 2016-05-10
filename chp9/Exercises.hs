0ca12f4f134f8c1511e34e54e6ef953159596165
pnaranja <paul.naranja@trilliantinc.com>
2016-05-10 16:50:43 -0700

    wip

--- chp9/Exercises.hs
+++ chp9/Exercises.hs
@@ -1,4 +1,13 @@
-Module Exercises where
+module Exercises where
 
 eftBool :: Bool -> Bool -> [Bool]
 eftBool x y = [x,y]
+
+eftOrd :: Ordering -> Ordering -> [Ordering]
+eftOrd = undefined
+
+eftInt :: Int -> Int -> [Int]
+eftInt = undefined
+
+eftChar :: Char -> Char -> [Char]
+eftChar = undefined
