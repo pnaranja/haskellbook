0036e1e261f6e09117b9ac714a3732418b932876
pnaranja <paul.naranja@gmail.com>
2016-05-11 00:27:32 -0700

    Refactored eftBool, eftOrd by creating instances of Num class

--- chp9/Exercises.hs
+++ chp9/Exercises.hs
@@ -1,10 +1,28 @@
 module Exercises where
 
+instance Num Bool where
+    (+) False 1 = True
+    fromInteger 0 = False
+    fromInteger 1 = True
+
 eftBool :: Bool -> Bool -> [Bool]
-eftBool x y = [x,y]
+eftBool start end
+ | start == end = [end]
+ | start > end = []
+ | otherwise = start : eftBool (start+1) end
+
+instance Num Ordering where
+    (+) LT 1 = EQ
+    (+) EQ 1 = GT
+    fromInteger 0 = LT
+    fromInteger 1 = EQ
+    fromInteger 2 = GT
 
 eftOrd :: Ordering -> Ordering -> [Ordering]
-eftOrd x y = [x,y]
+eftOrd start end
+ | start == end = [end]
+ | start > end = []
+ | otherwise = start : eftOrd (start+1) end 
 
 eftInt :: Int -> Int -> [Int]
 eftInt start end
