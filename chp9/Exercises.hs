87c505ff6187b4fada3d24df8b1232420fe702cc
pnaranja <paul.naranja@gmail.com>
2016-05-11 00:42:37 -0700

    Finished eft* exercise

--- chp9/Exercises.hs
+++ chp9/Exercises.hs
@@ -1,9 +1,17 @@
 module Exercises where
 
+import Data.Char
+
 instance Num Bool where
     (+) False 1 = True
+    (+) _ _ = undefined
+    (-) = undefined
+    (*) = undefined
+    abs = undefined
+    signum = undefined
     fromInteger 0 = False
     fromInteger 1 = True
+    fromInteger _ = undefined
 
 eftBool :: Bool -> Bool -> [Bool]
 eftBool start end
@@ -14,9 +22,15 @@ eftBool start end
 instance Num Ordering where
     (+) LT 1 = EQ
     (+) EQ 1 = GT
+    (+) _ _ = undefined
+    (-) = undefined
+    (*) = undefined
+    abs = undefined
+    signum = undefined
     fromInteger 0 = LT
     fromInteger 1 = EQ
     fromInteger 2 = GT
+    fromInteger _ = undefined
 
 eftOrd :: Ordering -> Ordering -> [Ordering]
 eftOrd start end
@@ -31,4 +45,8 @@ eftInt start end
  | otherwise = start : eftInt (start+1) end
 
 eftChar :: Char -> Char -> [Char]
-eftChar = undefined
+eftChar start end
+ | start == end = [end]
+ | start > end = []
+ | otherwise = start : eftChar (incChar start) end
+ where incChar = chr . (+1) . ord
