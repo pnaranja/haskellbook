9c70f968ca1cfef960ec4ec1f30f7adf07656ebb
pnaranja <paul.naranja@trilliantinc.com>
2016-05-24 00:02:02 -0700

    Working on Exercises: Understanding Folds

--- Chp10/Exercises.hs
+++ Chp10/Exercises.hs
@@ -24,10 +24,45 @@ myFoldr :: (a->b->b) -> b -> [a] -> b
 myFoldr f acc [] = acc
 myFoldr f acc (x:xs) = f x (myFoldr f acc xs)
 
--- myFoldr (-) 1 [3,4] == 3-(4-1) = 0
+-- myFoldr (-) 1 [3,4] == 3-(4-1) == 0
 --
 -- If f doesn't evaluate the second arg "(myFoldr f acc xs)"
 -- it can avoid the lists values and spine as well
 -- let u = undefined
 -- foldr (+) 0 (take 4 ([1,2,3,4] ++ u)) == 10
 -- foldr (+) 0 (take 2 (take 10 ([1,2,3,4] ++ u))) == 3  //Doesn't matter if take 10 bottoms!
+-- foldr (\_ _ -> 9001) 0 [undefined, undefined] == 9001 !!
+-- foldr const 0 [1..3] == const 1 (const 2 (const 3 0)) == const 1 _ == 1
+-- foldr const 0 [undefined, 2] == const undefined (const 2 0) == const undefined _ = Exception
+
+myFoldl :: (b->a->b) -> b -> [a] -> b
+myFoldl f acc [] = acc
+myFoldl f acc (x:xs) = myFoldl f (f acc x) xs
+
+-- myFoldl (-) 1 [3,4] == (1-3)-4 == -6
+-- Scans list all the intermediate steps of a fold
+-- scanl (-) 1 [3,4] == [1,-2,-6]
+--
+-- last (scanl f z xs) == foldl f z xs
+-- head (scanr f z xs) == foldr f z xs
+
+-- Folds as substitutions
+-- [1..3]           == 1 : 2 : 3 : []
+-- foldr f z [1..3] == 1 'f' (2 'f' (3 'f' z))
+--
+-- But not really with fold left
+-- foldl f z [1..3] == (((z 'f' 1) 'f' 2) 'f' 3)
+--
+-- foldl (flip (:)) [] [1..3] == ((([] ('flip' (:)) 1) ('flip' (:)) 2) ('flip' (:)) 3)
+--  == (((3 : 2) : 1) : []) == [3,2,1]
+--
+--
+-- Exercises: Understanding Folds
+-- 1. foldr (*) 1 [1..5] == foldl (*) 1 [1..5]
+-- 2. foldl (flip (*)) 1 [1..3] == (((3 * 2) * 1) *1)
+-- 3. foldr, but not foldl, associates to the right
+-- 4. Catamorphisms reduce structure
+-- 5. a) foldr (++) "WOOT" ["woot", "WOOT", "woot"]
+--    b) foldr max 'z' "fear is the little death" == 'z'
+--    c) foldr (:) [True] [False, True] == [False,True,True]
+--    d) Always returns True?
