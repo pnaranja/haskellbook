f665c3fd6c08adceeb9639764c0ec379916f3814
pnaranja <paul.naranja@trilliantinc.com>
2016-07-06 23:21:47 -0700

    Started Chp 11

---
+++ chp11/Exercises.hs
@@ -0,0 +1,30 @@
+module Exercises where
+
+{- 
+    Given:
+    data DogueDeBordeaux doge = DogueDeBordeaux doge
+
+    data Doggies a =
+    Husky a
+    | Mastiff a
+    deriving (Eq, Show) 
+
+    1. Is Doggies a type constructor or a data constructor?
+        Doggies is a type constructor
+    2. What is the kind of Doggies?
+        * -> *
+    3. What is the kind of Doggies String?
+        * -> *
+    4. What is the type of Husky 10?
+        Num a => Doggies a
+    5. What is the type of Husky (10 :: Integer)?
+        Integer a => Doggies a
+    6. What is the type of Mastiff "Scooby Doo"?
+        Doggies String
+    7. Is DogueDeBordeaux a type constructor or a data constructor?
+        Both
+    8. What is the type of DogueDeBordeaux?
+        doge -> DogueDeBordeaux doge
+    9. What is the type of DogueDeBordeaux "doggie!"
+        DogueDeBordeaux String
+-}
