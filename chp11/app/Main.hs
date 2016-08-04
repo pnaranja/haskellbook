51b6d944406aa8cf6fcf0f02061d92a2a19d7442
pnaranja <paul.naranja@gmail.com>
2016-08-03 22:05:03 -0700

    In Progress 11.12

--- chp11/app/Main.hs
+++ chp11/app/Main.hs
@@ -358,3 +358,106 @@ isDairyFarmerRec farmer =
 --  a and b is Bool, then inhabitants is 4
 --  True->True, True->False, False->True, False->False
 -- The -> operator, in the algebra of types, is an exponential operator
+
+data Quantum = Yes | No | Both deriving (Eq,Show)
+
+-- 3 + 3 (or 3*2 ?)
+--Sum Types
+--In Prelude: data Either a b = Left a | Right b
+quantSum1 :: Either Quantum Quantum
+quantSum1 = Right Yes
+
+quantSum2 :: Either Quantum Quantum
+quantSum2 = Right No
+
+quantSum3 :: Either Quantum Quantum
+quantSum3 = Right Both
+
+quantSum4 :: Either Quantum Quantum
+quantSum4 = Left Yes
+
+quantSum5 :: Either Quantum Quantum
+quantSum5 = Left No
+
+quantSum6 :: Either Quantum Quantum
+quantSum6 = Left Both
+
+
+-- 3 * 3
+-- Product types
+-- You can think of tuple (a,b) as: data MyTuple a b = MyTuple a b
+quantProd1 :: (Quantum,Quantum)
+quantProd1 = (Yes,Yes)
+
+quantProd2 :: (Quantum,Quantum)
+quantProd2 = (Yes,No)
+
+quantProd3 :: (Quantum,Quantum)
+quantProd3 = (Yes,Both)
+
+quantProd4 :: (Quantum,Quantum)
+quantProd4 = (No,Yes)
+
+quantProd5 :: (Quantum,Quantum)
+quantProd5 = (No,No)
+
+quantProd6 :: (Quantum,Quantum)
+quantProd6 = (No,Both)
+
+quantProd6 :: (Quantum,Quantum)
+quantProd6 = (Both,Yes)
+
+quantProd7 :: (Quantum,Quantum)
+quantProd7 = (Both,No)
+
+quantProd8 :: (Quantum,Quantum)
+quantProd8 = (Both,Both)
+
+-- 3^3
+-- Function types
+quantFlip1 :: Quantum -> Quantum
+quantFlip1 Yes = Yes
+quantFlip1 No = Yes
+quantFlip1 Both = Yes
+
+quantFlip2 :: Quantum -> Quantum
+quantFlip2 Yes = Yes
+quantFlip2 No = Yes
+quantFlip2 Both = No
+quantFlip3 :: Quantum -> Quantum
+quantFlip3 Yes = Yes
+quantFlip3 No = Yes
+quantFlip3 Both = Both
+
+quantFlip4 :: Quantum -> Quantum
+quantFlip4 Yes = Yes
+quantFlip4 No = No
+quantFlip4 Both = Yes
+quantFlip5 :: Quantum -> Quantum
+quantFlip5 Yes = Yes
+quantFlip5 No = Both
+quantFlip5 Both = Yes
+
+quantFlip6 :: Quantum -> Quantum
+quantFlip6 Yes = No
+quantFlip6 No = Yes
+quantFlip6 Both = Yes
+quantFlip7 :: Quantum -> Quantum
+quantFlip7 Yes = Both
+quantFlip7 No = Yes
+quantFlip7 Both = Yes
+
+quantFlip8 :: Quantum -> Quantum
+quantFlip8 Yes = Both
+quantFlip8 No = Both
+quantFlip8 Both = Both
+quantFlip9 :: Quantum -> Quantum
+quantFlip9 Yes = Both
+quantFlip9 No = Both
+quantFlip9 Both = Yes
+quantFlip10 :: Quantum -> Quantum
+quantFlip10 Yes = Both
+quantFlip10 No = Both
+quantFlip10 Both = No
+-- and so on up to quantFlip27...
+
