a31190a2f1077940f011c58c27b097c10a2de158
pnaranja <paul.naranja@gmail.com>
2017-01-04 23:44:04 -0800

    Start Phone exercise

--- chp11/app/Main.hs
+++ chp11/app/Main.hs
@@ -836,3 +836,32 @@ capitalizeWords = map (\str -> (capitalizeFirstLetter str, str)) . words
 
 capitalizeFirstLetter :: String -> String
 capitalizeFirstLetter (x:xs) = toUpper x : xs
+
+
+--Phone Exercise
+data DaPhone = DaPhone {key :: Char, letters :: String} deriving (Eq,Show)
+
+convo :: [String] 
+convo =
+       ["Wanna play 20 questions",
+        "Ya",
+        "U 1st haha",
+        "Lol ok. Have u ever tasted alcohol lol",
+        "Lol ya",
+        "Wow ur cool haha. Ur turn",
+        "Ok. Do u think I am pretty Lol",
+        "Lol ya",
+        "Haha thanks just making sure rofl ur turn"]
+
+-- Valid buttons = "1234567890*#"
+type Digit = Char
+
+-- Valid presses: 1 and up
+type Presses = Int
+
+-- assuming the default phone definition
+-- 'a' -> [('2', 1)]
+-- 'A' -> [('*', 1), ('2', 1)]
+reverseTaps :: DaPhone -> Char -> [(Digit, Presses)]
+reverseTaps = undefined
+
