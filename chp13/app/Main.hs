d5bb55f5cb4e01c29707d062ffda00bc325ba5c3
pnaranja <paul@studyblue.com>
2017-05-23 22:36:28 -0700

    Finished fillInChar and handleGuess functions

--- chp13/app/Main.hs
+++ chp13/app/Main.hs
@@ -11,6 +11,8 @@ import Data.List (intersperse)
 import System.Exit (exitSuccess)
 import System.Random (randomRIO)
 
+import Data.Monoid
+
 main :: IO ()
 main = do
   hSetBuffering stdout NoBuffering
@@ -57,6 +59,9 @@ getRandomWord = randomWord =<< gameWords
 -- 3. Characters user has chosen so far
 data Puzzle = Puzzle String [Maybe Char] [Char]
 
+-- How to show the puzzle on the command line
+-- First show the string with any correct guesses
+-- Then show all the letters that have been guessed
 instance Show Puzzle where
     show (Puzzle _ discovered guessed) =
         "\n" ++ (intersperse ' ' $ fmap renderPuzzleChar discovered) 
@@ -74,3 +79,28 @@ alreadyGuessed (Puzzle _ _ str) char = elem char str
 renderPuzzleChar :: Maybe Char -> Char
 renderPuzzleChar Nothing = '_'
 renderPuzzleChar (Just a) = a
+
+-- For fillInCharacter function
+instance Monoid Char where
+    mempty = ' '
+    mappend m1 m2 = m1
+
+-- Insert a CORRECTLY GUESSED character in the string
+-- (fill in the '_' character(s)
+-- Find out what matches in str, combine that list with the original discovered, and add char to guessed
+-- Using instance of Monoid for characters defined above
+fillInCharacter :: Puzzle -> Char -> Puzzle
+fillInCharacter (Puzzle str discovered guessed) char = Puzzle str newdiscovered newguessed
+    where
+        mapdiscovered = map (\c->if (c==char) then Just char else Nothing) str
+        newdiscovered = zipWith mappend mapdiscovered discovered
+        newguessed = guessed ++ [char]
+
+
+-- Handle a guess from the user
+-- This function will be used within the IO monad
+handleGuess :: Puzzle -> Char -> IO Puzzle
+handleGuess p@(Puzzle str discovered guessed) char
+  | alreadyGuessed p char == True = putStrLn ("You already guessed " ++ [char]) >>= (\x->return p)
+  | charInWord p char == True =putStrLn "You guessed correctly!" >>= (\x->return (fillInCharacter p char))
+  | otherwise = putStrLn "Incorrect!" >>= \x->return $ Puzzle str discovered (guessed ++ [char])
