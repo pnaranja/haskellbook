7c3d5398d874e6994f700b4cc8b3517936be4865
pnaranja <paul@studyblue.com>
2017-06-06 18:57:43 -0700

    Started QuickCheck exercises

--- chp14/src/Main.hs
+++ chp14/src/Main.hs
@@ -1,6 +1,7 @@
 module Main where
 
 import Test.Hspec
+import Test.QuickCheck
 
 main :: IO ()
 main = hspec $ do
@@ -35,6 +36,33 @@ testSummation = hspec $ do
 
 testSummation2 = hspec $ do
     describe "Test Summation" $ 
-        (it "5 times 20 is 100" (summation 5 20 `shouldBe` 100)) 
+        (it "5 times 20 is 100" $ summation 5 20 `shouldBe` 100) 
         >>= (\x->it "3 times 0 is 0" $ summation 3 0 `shouldBe` 0)
         >>= (\x->it "1 times 10000 is 10000" $ summation 1 10000 `shouldBe` 10000)
+
+
+testQuick1 = hspec $ do
+    describe "Testing QuickCheck" $ do
+        it "x + 1 is greater than x" $
+            property $ \x-> x + 1 > (x::Int)
+        it "x - 1 is less than x" $
+            property $ \x-> x - 1 < (x::Int)
+
+-- QuickCheck relies on typeclass Arbitrary and newtype Gen generating random data
+-- Use sample to see the random data
+-- sample (arbitary :: Gen Int)
+
+-- A trivial random generator of 1's , 1,2,3's and a-z
+-- run: sample' trivialGenInt
+trivialGenInt :: Gen Int
+trivialGenInt = return 1
+
+oneTwoThree :: Gen Int
+oneTwoThree = elements [1,2,3,3,3,2,2,2,2,1]
+
+genLetters :: Gen Char
+genLetters = elements ['a'..'z']
+
+-- Use choose to randomly choose from a tuple
+genBool :: Gen Bool
+genBool = choose (True,False)
