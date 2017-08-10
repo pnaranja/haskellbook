4af99cdf39cea0dde6bd4ceb8170fa13f2bb1288
pnaranja <paul@studyblue.com>
2017-08-09 21:38:22 -0700

    Chp15 wip

--- chp15/src/Main.hs
+++ chp15/src/Main.hs
@@ -47,3 +47,28 @@ t6 = mappend (Sum 2.5) (Sum 6.6)
 -- "Hello Paul"
 -- Data.Monoid> mappend "Hello" " Paul"
 -- "Hello Paul"
+
+t7 = mappend (Sum 8) (mappend (Sum 2) (Sum 5))
+t8 = Sum 8 <> Sum 2 <> Sum 5
+t9 = mconcat [Sum 8,Sum 2,Sum 5]
+t10 = getSum $ mconcat [Sum 8,Sum 2,Sum 5]
+
+-- Monoid Laws
+-- Identity
+t11 = (Sum 1) <> mempty
+t12 = mempty <> (Sum 1) 
+
+-- Associativity
+t13 = Sum 1 <> (Sum 2 <> Sum 3)
+t14 = (Sum 1 <> Sum 2) <> Sum 3
+
+-- Boolean Monoids
+t15 = All True <> All True <> All False
+t16 = Any True <> Any False <> Any False
+
+-- First and Last Monoids
+t17 = First (Just 1) <> First (Just 2)
+t18 = Last (Just 1) <> Last (Just 2)
+t19 = First (Nothing) <> First (Just 2)
+t20 = Last (Just 1) <> Last (Nothing)
+t21 = Last (Nothing) <> Last (Nothing)
